cb1d0e50777f66b2e7e45a20cd0dcada5251ea41
nileshbankar <nileshsbankar@gmail.com>
2021-06-25 11:17:57 +0530

    first commit

---
+++ src/app.js
@@ -0,0 +1,74 @@
+const path = require("path");
+const express = require("express");
+const hbs = require("hbs");
+
+const publicPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialPath = path.join(__dirname, "../templates/partials");
+
+const app = express();
+
+app.set("view engine", "hbs");
+app.set("views", viewsPath);
+hbs.registerPartials(partialPath);
+
+app.use(express.static(publicPath));
+
+app.get("", (req, resp) => {
+  resp.render("index", {
+    title: "Weather APP",
+    name: "Nilesh Bankar",
+  });
+});
+app.get("/about", (req, resp) => {
+  resp.render("about", {
+    title: "About",
+    name: "Nilesh Bankar",
+  });
+});
+
+app.get("/help", (req, resp) => {
+  resp.render("help", {
+    title: "Help",
+    name: "Nilesh Bankar",
+  });
+});
+
+app.get("/weather", (req, resp) => {
+  if (!req.query.address) {
+    return resp.send({
+      error: "You must provide address to fetch data",
+    });
+  }
+  resp.send({
+    Forecast: "Weather",
+    Location: "Pune",
+    address: req.query.address,
+  });
+});
+
+app.get("*", (req, resp) => {
+  resp.render("404", {
+    title: "404",
+    errorMessage: "404 ,Page you are looking does not exist",
+    name: "Nilesh Bankar",
+  });
+});
+// app.get("", (req, resp) => {
+//   resp.send("Hello world! It is my first attempt on node.js express server");
+// });
+
+// app.get("/about", (req, resp) => {
+//   resp.send("I am Nilesh Bankar, software engineer");
+// });
+
+// app.get("/help", (req, resp) => {
+//   resp.send("help");
+// });
+
+app.get("/weather", (req, resp) => {
+  resp.send({ location: "Pune", forecast: "Its raining" });
+});
+app.listen(3000, () => {
+  console.log("Server is up on port 3000");
+});
---
+++ src/utils/forecast.js
@@ -0,0 +1,29 @@
+const request = require("request");
+
+const forecast = (latitude, longitude, callback) => {
+  const url =
+    "http://api.weatherstack.com/current?access_key=0b24cc9fff9fd387609fb7384a838288/" +
+    latitude +
+    "," +
+    longitude;
+
+  request({ url, json: true }, (error, { body }) => {
+    if (error) {
+      callback("Unable to connect to weather service!", undefined);
+    } else if (body.error) {
+      callback("Unable to find location", undefined);
+    } else {
+      callback(
+        undefined,
+        body.daily.data[0].summary +
+          " It is currently " +
+          body.currently.temperature +
+          " degress out. There is a " +
+          body.currently.precipProbability +
+          "% chance of rain."
+      );
+    }
+  });
+};
+
+module.exports = forecast;
